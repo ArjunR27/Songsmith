17aad57eed916272453aa6342566ee9141c9d251
ArjunR27 <epicarjun27@gmail.com>
2024-04-30 19:06:57 -0700

    song database rudimentary implementation

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,17 +1,29 @@
 // backend.js
 import express from "express";
+import cors from "cors";
+import songServices from "./song-services.js"
 
 const app = express();
 const port = 8000;
 
+app.use(cors())
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+
+
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
   );
+});
+
+
+app.get("/songs", async (req, res) => {
+  const song_name = req.query["name"];
+  const result = await songServices.getSongs(song_name)
+  res.send({ song_list : result })
 });
---
+++ packages/express-backend/song-services.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+import fs from "fs";
+import songModel from "./song.js";
+
+mongoose.set("debug", true);
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/songs", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(async () => {
+    // Clear the database
+    console.log("Database cleared successfully.");
+
+    // Read JSON file
+    const jsonData = fs.readFileSync("./songs.json");
+    const songsData = JSON.parse(jsonData);
+
+    // Iterate over each song data
+    for (const songData of songsData) {
+        try {
+            // Check if the song already exists in the database
+            const existingSong = await songModel.findOne({
+                name: songData.track_name,
+                artist: songData.artist_name,
+                album: songData.album_name
+            });
+
+            // If the song doesn't exist, create and save a new Song document
+            if (!existingSong) {
+                const song = new songModel({
+                    name: songData.track_name,
+                    artist: songData.artist_name,
+                    album: songData.album_name,
+                    duration: songData.track_duration_min,
+                    image_link: songData.album_image_url
+                });
+                await song.save();
+            }
+        } catch (error) {
+            console.error(`Error adding song to the database: ${error}`);
+        }
+    }
+}).catch((error) => {
+    console.error(`Error connecting to MongoDB: ${error}`);
+});
+
+function findSongByName(name) {
+    return songModel.find({ name: name });
+}
+
+function getSongs(name) {
+    let promise;
+    if (name == undefined) {
+        promise = songModel.find();
+    }
+    else {
+        promise = findSongByName(name);
+    }
+    return promise; 
+}
+
+export default {
+    getSongs,
+}
