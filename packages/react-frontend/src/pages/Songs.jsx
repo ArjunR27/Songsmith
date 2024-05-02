6be3982dd6f78cabec03470e6a758bef09ff6274
hahns_pena <hpena02@calpoly.edu>
2024-05-02 16:51:56 -0700

    Basic pages

---
+++ packages/react-frontend/src/pages/Songs.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+//import "./Home.css"
+
+export default function Songs() {
+
+    return (
+        <>
+            <div className="songs">
+                These are songs!
+            </div>
+        </>
+    )
+}
