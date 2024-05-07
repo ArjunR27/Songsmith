0ae02260eca13904e6aab636581b8d87edcf4182
hahns_pena <hpena02@calpoly.edu>
2024-05-06 22:20:36 -0700

    Basic Table with CSS

--- packages/react-frontend/src/pages/Songs.jsx
+++ packages/react-frontend/src/pages/Songs.jsx
@@ -1,8 +1,19 @@
 
 import React, {useState} from "react"
-import '../App.css'
 import Table from "./SongsTable.jsx"
-//import "./Home.css"
+import './Songs.css'
+
+function MenuBar() {
+    return (
+        <div className="menu-bar">
+            <h1>Menu</h1> 
+
+            <h3>Songs</h3>
+            <h3>Playlists</h3>
+   
+        </div>
+    );
+}
 
 function Songs() {
     const [songs, setSongs] = useState([
@@ -26,21 +37,67 @@ function Songs() {
             "album_name": "GUTS",
             "track_duration_min": 2.575266666666667,
             "album_image_url": "https://i.scdn.co/image/ab67616d0000b273e85259a1cae29a8d91f2093d"
-        }
+        },
+        {
+            "track_name": "honey",
+            "artist_name": "Coastal Club",
+            "album_name": "honey",
+            "track_duration_min": 4.171783333333333,
+            "album_image_url": "https://i.scdn.co/image/ab67616d0000b273b02938092a02e50652ebab2e"
+        },
+        {
+            "track_name": "Good Luck, Babe!",
+            "artist_name": "Chappell Roan",
+            "album_name": "Good Luck, Babe!",
+            "track_duration_min": 3.6403833333333333,
+            "album_image_url": "https://i.scdn.co/image/ab67616d0000b27391b4bc7c88d91a42e0f3a8b7"
+        },
+        {
+            "track_name": "Read your Mind",
+            "artist_name": "Sabrina Carpenter",
+            "album_name": "emails i can't send",
+            "track_duration_min": 3.46415,
+            "album_image_url": "https://i.scdn.co/image/ab67616d0000b273700f7bf79c9f063ad0362bdf"
+        },
+        {
+            "track_name": "all-american bitch",
+            "artist_name": "Olivia Rodrigo",
+            "album_name": "GUTS (spilled)",
+            "track_duration_min": 2.7638833333333332,
+            "album_image_url": "https://i.scdn.co/image/ab67616d0000b2734063d624ebf8ff67bc3701ee"
+        },
+        {
+            "track_name": "the way things go",
+            "artist_name": "beabadoobee",
+            "album_name": "the way things go",
+            "track_duration_min": 3.13,
+            "album_image_url": "https://i.scdn.co/image/ab67616d0000b2739c835164f2d56dae7ee8a402"
+        },
+        {
+            "track_name": "End of Beginning",
+            "artist_name": "Djo",
+            "album_name": "DECIDE",
+            "track_duration_min": 2.6540833333333333,
+            "album_image_url": "https://i.scdn.co/image/ab67616d0000b273fddfffec51b4580acae727c1"
+        },
     ]);
 
     return (
         <>
-            <div className="App">
-                Songs
-                    
+            <div className="navTop"> 
+                <h1>Songsmith</h1>
+            </div>
                 
+            <div className="table-container">
+                <Table songData={songs} />
+            </div>
 
-                <Table 
-                    songData={songs} 
-                />
-            </div> 
+            <MenuBar></MenuBar>
         </>
+        
+            
+          
+  
 
     )
 }
--- packages/react-frontend/src/pages/SongsTable.jsx
+++ packages/react-frontend/src/pages/SongsTable.jsx
@@ -1,13 +1,15 @@
+import './Songs.css'
 
 function TableHeader() {
     return (
         <thead>
             <tr>
-            <th>Track Name</th>
-            <th>Artist Name</th>
-            <th>Album Name</th>
-            <th>Track Duration</th>
-            <th>Album Image</th>
+                <th>#</th>
+                <th>Album Image</th>
+                <th>Track Name</th>
+                <th>Artist Name</th>
+                <th>Album Name</th>
+                <th>Track Duration</th>
             </tr>
         </thead>
     );
@@ -21,11 +23,13 @@ function TableBody(props) {
     const rows = props.songData.map((row, index) => {
         return (
             <tr key={index}>
+                <td>{1 + index}</td>
+                <td><img src={row.album_image_url} alt="Album Image" style={{ width: '100px', height: '100px' }} /></td>
                 <td>{row.track_name}</td>
                 <td>{row.artist_name}</td>
                 <td>{row.album_name}</td>
                 <td>{convertMins(row.track_duration_min)}</td>
-                <td><img src={row.album_image_url} alt="Album Image" style={{ width: '100px', height: '100px' }} /></td>
+                
                 
             </tr>
         );
@@ -39,15 +43,18 @@ function TableBody(props) {
 
 }
 
+
 function Table(props) {
     return (
-        <table>
-            <TableHeader/>
-            <TableBody
-                songData={props.songData}
-            />
+        <>
+            <h1 className = "song-header">Songs</h1>
+            <table className="song-table">
+                <TableHeader />
+                <TableBody songData={props.songData} />
+            </table>
+        </>
             
-        </table>
+       
         
     );
 }
