69aae9de68bb963c130435e5f155603a41cf8f2c
hahns_pena <hpena02@calpoly.edu>
2024-05-20 17:02:29 -0700

    Landing Page Init

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -1,38 +1,43 @@
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import './App.css';
+import Home from "./pages/Home.jsx";
+import Songs from "./pages/Songs.jsx";
+import Playlists from "./pages/Playlists.jsx";
+import Playlist from "./pages/Playlist.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Sidebar from './components/SideBar';
+import React from 'react';
+import Header from './components/Header.jsx';
 
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css'
-import Home from "./pages/Home.jsx"
-import Songs from "./pages/Songs.jsx"
-import Playlists from "./pages/Playlists.jsx"
-import Playlist from "./pages/Playlist.jsx"
-import Sidebar from './components/SideBar'
-import React from 'react'
-import Header from './components/Header.jsx'
+function AppContent() {
+  const location = useLocation();
+  const hideHeaderAndSidebar = location.pathname === "/" || location.pathname === "/login" || location.pathname === "/signup";
+
+  return (
+    <>
+      {!hideHeaderAndSidebar && <Sidebar />}
+      {!hideHeaderAndSidebar && <Header />}
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/playlists" element={<Playlists />} />
+        <Route exact path="/playlists/:id" element={<Playlist />} />
+        <Route exact path="/songs" element={<Songs />} />
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-       <BrowserRouter>    
-          <Sidebar />
-          <Header />       
-            <Routes>
-                <Route exact path="/" element={<Home />}>
-                </Route>
-                <Route exact path="/playlists" element={<Playlists/>}>
-                </Route>
-                <Route exact path="/playlists/:id" element={<Playlist />} >
-                </Route>
-                <Route exact path="/songs" element={<Songs />}>
-                </Route>
-            </Routes>
-        </BrowserRouter>
-         
+      <BrowserRouter>
+        <AppContent />
+      </BrowserRouter>
     </div>
-    
-  )
-
+  );
 }
 
 export default App;
-
---
+++ packages/react-frontend/src/pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+//import "./Home.css"
+
+export default function Signup() {
+    return (
+        <>
+            <div className="signup">
+                Signup
+            </div>
+        </>
+    )
+}
