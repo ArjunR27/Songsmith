2025fd9293fdc42cf83d0a7f27e958e77bdfc147
hahns_pena <hpena02@calpoly.edu>
2024-06-06 01:29:27 -0700

    Create Song Bug Fix

--- packages/react-frontend/src/pages/CreateSong.jsx
+++ packages/react-frontend/src/pages/CreateSong.jsx
@@ -1,5 +1,6 @@
 import "./CreateSong.css";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import InputFields from "../components/inputFields";
 
 function CreateSong() {
@@ -8,6 +9,7 @@ function CreateSong() {
   const [album, setAlbum] = useState("");
   const [duration, setDuration] = useState(0);
   const [songImg, setImg] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -32,77 +34,79 @@ function CreateSong() {
         return response.json();
       })
       .then(() => {
-        window.location.href = "/songs";
+        setIsSubmitted(true);
       })
       .catch((error) => {
         console.error("Error:", error);
       });
   };
 
+  if (isSubmitted) {
+    return <Link to="/songs">Go to Songs</Link>;
+  }
+
   return (
-    <>
-      <div className="page">
-          <h1>Add a Song</h1>
-          <div className="addSong">
-            <form onSubmit={handleSubmit}>
-              <InputFields
-                type="text"
-                placeholder="Song"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                id="name"
-                labelName="Song Name: "
-              />
-              <InputFields
-                type="text"
-                placeholder="Artist"
-                value={artist}
-                onChange={(e) => setArtist(e.target.value)}
-                id="Artist"
-                labelName="Artist: "
-              />
-              <InputFields
-                type="text"
-                placeholder="Album"
-                value={album}
-                onChange={(e) => setAlbum(e.target.value)}
-                id="Album"
-                labelName="Album: "
-              />
-              <InputFields
-                type="number"
-                placeholder="Duration(mins)"
-                value={duration}
-                onChange={(e) => setDuration(parseFloat(e.target.value))}
-                id="Duration"
-                labelName="Duration: "
-              />
-              <InputFields
-                type="text"
-                placeholder="Image url"
-                value={songImg}
-                onChange={(e) => setImg(e.target.value)}
-                id="Img"
-                labelName="Song Cover: "
-              />
-              <button
-                type="submit"
-                style={{
-                  color: "white",
-                  padding: "10px 20px",
-                  border: "none",
-                  borderRadius: "900px",
-                  cursor: "pointer",
-                  fontSize: "20px",
-                  marginTop: "50px",
-                }}
-              >
-                Create Song
-              </button>
-            </form>
-          </div>
+    <div className="page">
+      <h1>Add a Song</h1>
+      <div className="addSong">
+        <form onSubmit={handleSubmit}>
+          <InputFields
+            type="text"
+            placeholder="Song"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            id="name"
+            labelName="Song Name: "
+          />
+          <InputFields
+            type="text"
+            placeholder="Artist"
+            value={artist}
+            onChange={(e) => setArtist(e.target.value)}
+            id="Artist"
+            labelName="Artist: "
+          />
+          <InputFields
+            type="text"
+            placeholder="Album"
+            value={album}
+            onChange={(e) => setAlbum(e.target.value)}
+            id="Album"
+            labelName="Album: "
+          />
+          <InputFields
+            type="number"
+            placeholder="Duration(mins)"
+            value={duration}
+            onChange={(e) => setDuration(parseFloat(e.target.value))}
+            id="Duration"
+            labelName="Duration: "
+          />
+          <InputFields
+            type="text"
+            placeholder="Image url"
+            value={songImg}
+            onChange={(e) => setImg(e.target.value)}
+            id="Img"
+            labelName="Song Cover: "
+          />
+          <button
+            type="submit"
+            style={{
+              color: "white",
+              padding: "10px 20px",
+              border: "none",
+              borderRadius: "900px",
+              cursor: "pointer",
+              fontSize: "20px",
+              marginTop: "50px",
+            }}
+          >
+            Create Song
+          </button>
+        </form>
       </div>
-    </>
+    </div>
   );
 }
 
